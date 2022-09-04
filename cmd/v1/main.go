5fa4b8c88a52fe19f314f29c6a6348e5087cf71a
gmarcha <gmarcha@github.com>
2022-09-04 09:38:41 +0200

    Add generated entrypoint from goswagger

---
+++ cmd/v1/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/go-openapi/loads"
+	flags "github.com/jessevdk/go-flags"
+
+	_ "github.com/gmarcha/notion-goswagger-api/internal/v1/env"
+	"github.com/gmarcha/notion-goswagger-api/internal/v1/goswagger/restapi"
+	"github.com/gmarcha/notion-goswagger-api/internal/v1/goswagger/restapi/operations"
+)
+
+func main() {
+
+	swaggerSpec, err := loads.Embedded(restapi.SwaggerJSON, restapi.FlatSwaggerJSON)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	api := operations.NewNotionAPI(swaggerSpec)
+	server := restapi.NewServer(api)
+	defer server.Shutdown()
+
+	parser := flags.NewParser(server, flags.Default)
+	parser.ShortDescription = "Notion API"
+	parser.LongDescription = "An API to automate content creation on Notion."
+	server.ConfigureFlags()
+	for _, optsGroup := range api.CommandLineOptionsGroups {
+		_, err := parser.AddGroup(optsGroup.ShortDescription, optsGroup.LongDescription, optsGroup.Options)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	if _, err := parser.Parse(); err != nil {
+		code := 1
+		if fe, ok := err.(*flags.Error); ok {
+			if fe.Type == flags.ErrHelp {
+				code = 0
+			}
+		}
+		os.Exit(code)
+	}
+
+	server.ConfigureAPI()
+
+	if err := server.Serve(); err != nil {
+		log.Fatalln(err)
+	}
+
+}
